a2114c77b5501aeb3e10edbe3ee6209f984b6576
xu <1245545667@qq.com>
2019-05-08 14:01:57 +0800

    React登录表单

---
+++ src/components/comment.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Comment extends Component {
+  state = {
+  };
+  render() {
+    return (
+      <div>1234</div>
+    );
+  }
+}
+
+
+export default Comment;
---
+++ src/containers/docs.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Documentation from '~/components/documentation';
+import Comment from '~/components/Comment';
+
+class Docs extends Component {
+  state = {
+  };
+  render() {
+    return (
+      <Documentation />
+      // <Documentation />
+    );
+  }
+}
+
+export default Docs;
